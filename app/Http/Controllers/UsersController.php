24d79335610dd78107e5dace1368ae40c88ed76c
lixiaode222 <1803118517@qq.com>
2018-09-18 13:20:39 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -19,8 +20,18 @@ class UsersController extends Controller
     }
 
     //处理用户信息修改逻辑
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
+    {
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
