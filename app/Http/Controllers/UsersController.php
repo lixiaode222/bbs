052d73e98f2787e89c658d5e2492066d23563b96
lixiaode222 <1803118517@qq.com>
2018-09-18 13:30:22 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    //权限过滤
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     //用户个人中心页面
     public function show(User $user){
         return view('users.show',compact('user'));
@@ -16,12 +22,14 @@ class UsersController extends Controller
 
     //用户编辑资料页面
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
 
     //处理用户信息修改逻辑
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
