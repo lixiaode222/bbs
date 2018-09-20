fa1f2e5463da102d0e1f2750ddeeb5b482496945
lixiaode222 <1803118517@qq.com>
2018-09-20 02:53:47 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //得到用户所有的话题
+    public function topics(){
+        return $this->hasMany(Topic::class);
+    }
 }
