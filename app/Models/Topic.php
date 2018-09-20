fa19df605dac763f6007ac314b143237eeb7277b
lixiaode222 <1803118517@qq.com>
2018-09-20 01:33:35 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //通过文章得到它对应的分类
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    //通过文章得到它对应的作者ID
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
